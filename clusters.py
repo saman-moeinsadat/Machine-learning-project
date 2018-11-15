9d923ab44cf2ba05be853c255cb31175be4b6cfd
saman <moein.saman@gmail.com>
2018-11-15 18:54:21 +0100

    constructing the dictionary and trying chapter two's algorythm

--- clusters.py
+++ clusters.py
@@ -1,3 +1,5 @@
+from ai_topMatches import topMatches
+
 def readFile(filename):
     lines = [line for line in open(filename)]
     colnames = lines[0].strip().split('\t')[1:]
@@ -9,13 +11,17 @@ def readFile(filename):
         rownames.append(p[0])
         data.append([float(x) for x in p[1:]])
 
-    return rownames,colnames,data
+    return rownames, colnames, data
 
 
 [r,c,d] = readFile('blogdata.txt')
 print(r)
-print(len(r))
-print(c)
-print(len(c))
-print(d)
-print(len(d))
+#print(c)
+#print(d)
+wordFrequency = {}
+for blog in r:
+    wordFrequency.setdefault(blog,{})
+    for word in c:
+        wordFrequency[blog][word] = d[r.index(blog)][c.index(word)]
+print(wordFrequency)
+print(topMatches(wordFrequency,'PaulStamatiou.com - Technology, Design and Photography'))
