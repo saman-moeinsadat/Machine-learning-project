943eec5c9ffd101c8b493f620890fdbb645a8e49
saman <moein.saman@gmail.com>
2018-11-16 15:54:31 +0100

    + K-Means clustering

--- clusters.py
+++ clusters.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from PIL import Image, ImageDraw
+import random
 
 def readFile(filename):
     lines = [line for line in open(filename)]
@@ -65,8 +66,8 @@ def hcluster(rows,distance = pearson):
 
     return clust[0]
 
-(blognames,words,data) = readFile('blogdata.txt')
-clust = hcluster(data)
+#(blognames,words,data) = readFile('blogdata.txt')
+#clust = hcluster(data)
 
 def printclust(clust, labels =None, n = 0):
     for i in range(n):
@@ -138,8 +139,8 @@ def drawdendrogram(clust,labels,jpeg='clusters.jpg'):
 
 
 blognames,words,data = readFile('blogdata.txt')
-clust = hcluster(data)
-drawdendrogram(clust,blognames,jpeg='blogclust.jpg')
+#clust = hcluster(data)
+#drawdendrogram(clust,blognames,jpeg='blogclust.jpg')
 
 def rotatematrix(data):
     newdata = []
@@ -149,6 +150,44 @@ def rotatematrix(data):
 
     return newdata
 
-rdata=rotatematrix(data)
-wordclust = hcluster(rdata)
-drawdendrogram(wordclust,labels=words,jpeg='wordclust.jpg')
+#rdata=rotatematrix(data)
+#wordclust = hcluster(rdata)
+#drawdendrogram(wordclust,labels=words,jpeg='wordclust.jpg')
+
+def kcluster(rows,distance=pearson, k=4):
+    ranges = [(min([row[i] for row in rows]),max([row[i] for row in rows])) for i in range(len(rows[0]))]
+
+    clusters = [[int(random.random()*(ranges[i][1] - ranges[i][0])) + ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+    lastmatches = None
+
+    for t in range(100):
+        print 'Iteration %d' % t
+        bestmatches = [[] for i in range(k)]
+
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i],row)
+                if d < distance(clusters[bestmatch],row):
+                    bestmatch =i
+            bestmatches[bestmatch].append(j)
+
+        if bestmatches == lastmatches:
+            break
+        lastmatches == bestmatches
+        for i in range(k):
+            avgs = [0.0]*len(rows[0])
+            if len(bestmatches[i])>0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m]+= rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j]/=len(bestmatches[i])
+                clusters[i] = avgs
+
+    return bestmatches
+
+k = kcluster(data)
+for i in range(len(k)):
+    print [blognames[id] for id in k[i]]
