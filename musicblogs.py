2386e15ac4b4ba2e23ffcf6c5aa623bb567f2fe7
saman <moein.saman@gmail.com>
2018-11-20 17:58:00 +0100

    producing the data matrix about diffrent music websites with the name of each website and the number of appearance of words in that website printed out on a text doc

--- musicblogs.py
+++ musicblogs.py
@@ -3,15 +3,28 @@ from bs4 import BeautifulSoup
 import re
 
 
-def getWords(feed):
+def getWords(soup):
+    content = soup.find('div',class_='mw-parser-output').find_all('p')
+    con = iter(content)
+    char = re.compile(r'\[.*?\]')
+    content_strip = char.sub('',str(next(con).get_text()))
+    while len(content_strip.split(' ')) < 30:
+        content_strip += char.sub('',str(next(con)))
+
+    list = content_strip.split(' ')
+    return [item.lower() for item in list]
+
+def getWordCount(feed):
+    wc = {}
     url = urllib2.urlopen(feed)
     soup1 = BeautifulSoup(url,'lxml')
-    content = soup1.find('div',class_='mw-parser-output').find('p').get_text()
-    char = re.compile(r'\[.*?\]')
-    content_strip = char.sub('',str(content))
     title = soup1.title.get_text()
+    words = getWords(soup1)
+    for word in words:
+        wc.setdefault(word,0)
+        wc[word]+=1
 
-    return title,content_strip
+    return title,wc
 
 website_url = urllib2.urlopen('https://en.wikipedia.org/wiki/Category:Music_websites')
 soup = BeautifulSoup(website_url, 'lxml')
@@ -37,19 +50,43 @@ for item in music_websites:
         print(item,e.code)
     except urllib2.URLError, e:
         print(item,e.args)
-length={}
+
+apcount={}
+wordcount = {}
 for item in validlist:
     try:
-        titl,words = getWords(item)
-        wordlist = words.split(' ')
-        length.setdefault(titl,0)
-        length[titl] = len(wordlist)
+        titl,words = getWordCount(item)
     except Exception:
         continue
+    wordcount[titl] = words
+    for w,c in words.items():
+        apcount.setdefault(w,0)
+        if c > 1:
+            apcount[w]+=1
 
-print length
-print len(length)
-print len(validlist)
+wordlist = []
 
-for item in length.values():
-    print item
+for w,bc in apcount.items():
+    frac = (float(bc)/len(validlist))*1000
+    if frac > 5 and frac <30:
+        wordlist.append(w)
+
+out = open('musicblogdata.txt','w')
+out.write('Musoc_Websites.txt')
+for word in wordlist:
+    out.write('\t%s' % word)
+out.write('\n')
+for blog,wc in wordcount.items():
+    try:
+        out.write(str(blog))
+        for word in wordlist:
+            if word in wc:
+                out.write('\t%d' % wc[word])
+            else:
+                out.write('\t0')
+    except ValueError:
+        continue
+    out.write('\n')
+
+print len(wordcount)
+print len(wordlist)
