97ae496f6e6f58832f8484daa9d61548e852ae18
saman <moein.saman@gmail.com>
2018-11-24 14:59:56 +0100

    going further with krawler class

--- searchengine.py
+++ searchengine.py
@@ -16,15 +16,33 @@ class crawler:
         self.con.commit()
 
     def getentryid(self, table, field, value, createnew=True):
-        return None
-
+        cur=self.con.execute("select rowid from %s where %s='%s'" %(table,field,value))
+        res = cur.fetchone()
+        if res==None:
+            cur = self.con.execute("insert into %s (%s) values('%s')" %(table,field,value))
+            return cur.lastrowid
+        else:
+            return res[0]
     def addtoindex(self, url, soup):
+        if self.isindexed(url):
+            return
         print 'Indexing %s ' % url
+
+        text = self.gettextonly(soup)
+        words = self.separatewords(text)
+
+        urlid = self.getentryid('urllist','url',url)
+        for i in range(len(words)):
+            word = words[i]
+            if word in ignorewords:
+                continue
+            wordid = self.getentryid('wordlist','word',word)
+            self.con.execute("insert into wordlocation values (:urlid,:wordid,:location)", {'urlid':urlid,'wordid':wordid,'location':i})
 
     def gettextonly(self, soup):
         v = soup.string
         if v == None:
-            c = v.content
+            c = soup.contents
             resulttext = ''
             for t in c:
                 subtext=self.gettextonly(t)
@@ -34,10 +52,15 @@ class crawler:
             return v.strip()
 
     def separatewords(self, text):
-        splitter = re.complie('\\W*')
-        return [s.lower() for s in splitter.split(text) if s!='']
+        self.splitter = re.compile('\\W*')
+        return [s.lower() for s in self.splitter.split(text) if s!='']
 
     def isindexed(self, url):
+        u = self.con.execute("select rowid from urllist where url='%s'" % url).fetchone()
+        if u!=None:
+            v = self.con.execute("select * from wordlocation where urlid=%d " %u[0]).fetchone()
+            if v!=None:
+                return True
         return False
 
     def addlinkref(self, urlFrom, urlTo, linkText):
@@ -91,5 +114,7 @@ wikifeeds=[]
 for line in open('musicWikiFeeds.txt'):
     wikifeeds.append(line.strip('\n'))
 
-# crawler = crawler('searchindex.db')
+crawler = crawler('searchindex.db')
 # crawler.createindextables()
+
+s = crawler.crawl(wikifeeds)
